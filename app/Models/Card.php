fe60e4fceab37f091e02450be37b5e750c672677
Samuel Owadayo <odevservices@gmail.com>
2023-10-06 07:01:33 +0100

    model relationship mapping

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Card extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'column_id',
+        'position',
+    ];  
+
+    public function column(): BelongsTo
+    {
+        return $this->belongsTo(Column::class);
+    }
 }
